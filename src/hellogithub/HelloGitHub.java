71c90c3f4374f473c5a494c7d6c1cc86c3711087
Smakovskyi <smakovskiy@ukr.net>
2015-04-23 17:59:21 +0300

    Update HelloGitHub.java

--- src/hellogithub/HelloGitHub.java
+++ src/hellogithub/HelloGitHub.java
@@ -18,6 +18,8 @@ public class HelloGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Git");
+        System.out.println("Hello From Git!");
+        
     }
     
 }
