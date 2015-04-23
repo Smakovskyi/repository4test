f553615c4b53a13479a5cd20be49946b777d5528
sds <sds@10.17.8.212>
2015-04-23 18:00:03 +0300

    next

--- src/hellogithub/HelloGitHub.java
+++ src/hellogithub/HelloGitHub.java
@@ -17,6 +17,7 @@ public class HelloGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Local Hello");
         System.out.println("Hello Git");
     }
     
